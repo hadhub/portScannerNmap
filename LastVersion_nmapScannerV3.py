b34e7e7c48f2a479ce852d88eef2da2dce70046d
hadhub <85348372+hadhub@users.noreply.github.com>
2021-06-06 00:30:20 +0200

    Update LastVersion_nmapScannerV3.py

--- LastVersion_nmapScannerV3.py
+++ LastVersion_nmapScannerV3.py
@@ -9,19 +9,19 @@ Add :
 [Design] Progess Bar (For V3) : OK
 
 Code :
-    List Functions : 
-        - 1 Print menu : Asking user about the IP & Range Port
-            + Convert hostname to IPV4 for scan 
-        - 1 Scanning (SYN/ACK Scan)
-         + Args : -sS
+    - 1 Print menu : Asking user about the IP & Range Port
+        + Convert hostname to IPV4 for scan 
+
+    - 1 Scanning (SYN/ACK Scan)
+        + Args : -sS
 """
 
 print("-" * 50)
-# Forcing du cast en str pour les liens ctrl+c & ctrl+v des navigateurs
+# Using str() when user copy & paste IP from web browser
 hostname = str(input("[?] Enter hostname : "))
 port_range = str(input("[?] Range port (default value is 0-1024) : "))
 print("-" * 50)
-# Vérification des entrées utilisateur
+# Verification of user inputs
 if port_range == '':
     port_range = "0-1024"
 else:
@@ -31,13 +31,13 @@ if hostname == '':
     print("[*] Enter valid hostname")
 else:
     print("[*] Hostname entered : ", hostname)
-# Hostname > ipV4 (socket)
+# Hostname to ipV4 (socket)
 ip_v4 = socket.gethostbyname(hostname)
 print("[*] IpV4 of hostname : ", ip_v4)
 print("-" * 50)
 
 #  ! Scan SYN/ACK
-# Instanciation de l'objet Scanner
+# Instantiation of the Scanner object
 Scanner = nmap.PortScanner()
 bar = Bar('Processing', max=3)
 for i in range(3):
@@ -46,15 +46,15 @@ for i in range(3):
 bar.finish()
 
 print("-" * 50)
-# Affichage sans cast (.state() retourne le status de l'adresse IP en une str)
+# Display without cast (.state() return the state of IP in str)
 ip_status = Scanner[ip_v4].state()
 print("[*] Ip Status : ", ip_status)
 
-# Affichage du protocol utilisé
+# Display of the used protocol
 protocol_list = Scanner[ip_v4].all_protocols()
 print("[*] Protocol : ", *protocol_list)
 
-# Affichage des ports ouverts
+# Display of open ports
 open_port = Scanner[ip_v4]['tcp'].keys()
 print("[*] Open Ports: ", *open_port)
 print("-" * 50)
